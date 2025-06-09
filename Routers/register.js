2caeee946b3157ee8a3134474d3fb022350b7c23
Seifeldeen Ali <111438242+sefoo333@users.noreply.github.com>
2025-06-09 21:24:33 +0300

    Update register.js

--- Routers/register.js
+++ Routers/register.js
@@ -9,13 +9,7 @@ const cors = require("cors")
 const website = "https://soblo-project.vercel.app"
 
 Router.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (website == origin) {
-      return callback(null, true);
-    }
-    return callback(new Error('Not allowed by CORS'));
-  },
+  origin:website,
   credentials: true, // إذا كنت تستخدم الكوكيز أو الجلسات
     exposedHeaders: ['x-auth-header'],         
 }));
